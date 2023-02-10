4836adcb07b8ea5d0bf30e79edd051a6ba313793
vinouno <1074543006@qq.com>
2023-02-10 19:16:35 +0800

    the question 1 of leetcode

---
+++ 1.两数之和.cpp
@@ -0,0 +1,30 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include <unordered_map>
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashtable;
+        for (int i = 0; i < nums.size(); ++i) {
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end()) {
+                return {it->second, i};
+            }
+            hashtable[nums[i]] = i;
+        }
+        return {};
+    }
+};
+// @lc code=end
+

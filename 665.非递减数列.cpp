33cede9676e21af53413818cb820582238bd4364
vinouno <1074543006@qq.com>
2023-03-09 20:28:46 +0800

    the question 665 of leetcode

---
+++ 665.非递减数列.cpp
@@ -0,0 +1,35 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=665 lang=cpp
+ *
+ * [665] 非递减数列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool checkPossibility(vector<int> &nums) {
+        int n = nums.size(), cnt = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            int x = nums[i], y = nums[i + 1];
+            if (x > y) {
+                cnt++;
+                if (cnt > 1) {
+                    return false;
+                }
+                if (i > 0 && y < nums[i - 1]) {
+                    nums[i + 1] = x;
+                }
+            }
+        }
+        return true;
+    }
+};
+// @lc code=end
+

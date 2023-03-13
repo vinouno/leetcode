5d4488647f100109e91baf00704c323c489a6b73
vinouno <1074543006@qq.com>
2023-03-13 20:37:57 +0800

    the question 150 of leetcode

---
+++ 239.滑动窗口最大值.cpp
@@ -0,0 +1,23 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include <queue>
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=239 lang=cpp
+ *
+ * [239] 滑动窗口最大值
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        queue<int> maxNum;
+    }
+};
+// @lc code=end
+

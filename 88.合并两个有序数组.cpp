00ad54e7a6e1c2c4161e3f25357875c29df86dff
vinouno <1074543006@qq.com>
2023-03-18 16:41:47 +0800

    the question 88 of leetcode

---
+++ 88.合并两个有序数组.cpp
@@ -0,0 +1,33 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include <queue>
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=88 lang=cpp
+ *
+ * [88] 合并两个有序数组
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = nums1.size() - 1;
+        m--;
+        n--;
+        while (n >= 0)
+        {
+            while (m >= 0 && nums1[m] > nums2[n])
+            {
+                swap(nums1[i--], nums1[m--]);
+            }
+            swap(nums1[i--], nums2[n--]);
+        }
+    }
+};
+// @lc code=end
+
